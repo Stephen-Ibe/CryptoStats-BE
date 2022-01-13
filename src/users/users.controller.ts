cdfa8346e0016cf0c8b607758e2409d3f7cca384
KalsonSaint <saintkalson@gmail.com>
2022-01-13 10:35:15 +0100

    Password Hash

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,16 @@
+import { UserResponse } from './dto/response/userResponse.dto';
 import { UsersService } from './users.service';
 import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserRequest } from './dto/createUserRequest.dto';
+import { CreateUserRequest } from './dto/request/createUserRequest.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Post()
-  async createUser(@Body() createUserRequest: CreateUserRequest): Promise<any> {
+  async createUser(
+    @Body() createUserRequest: CreateUserRequest,
+  ): Promise<UserResponse> {
     return this.userService.createUser(createUserRequest);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,29 @@
-import { CreateUserRequest } from './dto/createUserRequest.dto';
 import { Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { CreateUserRequest } from './dto/request/createUserRequest.dto';
+import { UserResponse } from './dto/response/userResponse.dto';
+import { User } from './models/User';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async createUser(createUserRequest: CreateUserRequest): Promise<any> {
-    return this.usersRepository.insertOne(createUserRequest);
+  async createUser(
+    createUserRequest: CreateUserRequest,
+  ): Promise<UserResponse> {
+    const user = await this.usersRepository.insertOne({
+      ...createUserRequest,
+      password: await hash(createUserRequest.password, 10),
+    });
+
+    return this.buildResponse(user);
+  }
+
+  private buildResponse(user: User): UserResponse {
+    return {
+      _id: user._id.toHexString(),
+      email: user.email,
+    };
   }
 }

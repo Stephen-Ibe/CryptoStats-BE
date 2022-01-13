82d6b97cb5315942ad3dff85f76c9ecdfec08164
KalsonSaint <saintkalson@gmail.com>
2022-01-13 10:04:43 +0100

    User controller, models, module and service setup && createUser API complete

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserRequest } from './dto/createUserRequest.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
   @Post()
-  async createUser(): Promise<any> {}
+  async createUser(@Body() createUserRequest: CreateUserRequest): Promise<any> {
+    return this.userService.createUser(createUserRequest);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,12 @@
+import { CreateUserRequest } from './dto/createUserRequest.dto';
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async createUser(createUserRequest: CreateUserRequest): Promise<any> {
+    return this.usersRepository.insertOne(createUserRequest);
+  }
+}
